f3ada77680f2dd0c736051c33116c5536ef54c52
joec11 <joseph.candela0@gmail.com>
2025-07-27 17:10:25 -0400

    Removed TEMPLATES_DIR

--- settings/config.py
+++ settings/config.py
@@ -1,8 +1,8 @@
-# Standard library imports
+# Standard Library Imports
 import os
 import json
 
-# Third-party imports
+# Third-Party Imports
 from dotenv import find_dotenv, load_dotenv
 
 class Settings:
@@ -18,9 +18,6 @@ class Settings:
 
     # Path to the logging configuration file, defaulting to 'logging.conf'
     LOGGING_CONFIG = os.getenv('LOGGING_CONFIG', 'logging.conf')
-
-    # Path to the templates directory, defaulting to 'templates'
-    TEMPLATES_DIR = os.getenv('TEMPLATES_DIR', 'templates')
 
     # Path to the index html file, defaulting to 'convert_images.html'
     INDEX_HTML = os.getenv('INDEX_HTML', 'convert_images.html')
